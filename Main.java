df99dd065da3f0a7d850f2812535f889750d47a1
Ivanov Vlad <lrn@2ch.hk>
2022-05-11 12:10:25 +0300

    initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+package structural.adapter;
+
+public class Main {
+    public static void main(String[] args) {
+        Ints intsCalc = new IntsCalculator();
+        System.out.println(intsCalc.sum(2, 2));
+        System.out.println(intsCalc.mult(3, 3));
+        System.out.println(intsCalc.pow(2, 10));
+    }
+}
